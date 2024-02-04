02ebe13845d93e8a64102a078bc08905a81f3615
shreemit <shreemit27@gmail.com>
2024-02-04 13:26:00 -0800

    Refactor user input and display top-rated and recommended movies

--- app.py
+++ app.py
@@ -14,29 +14,44 @@ st.title('Movie Recommender System')
 st.subheader('Welcome to the Movie Recommender System built by Shreemit')
  
 # check if userid is numeric or else return error
-user_id = st.text_input('Enter User ID here:')
-if user_id.isnumeric() and int(user_id) > 0:
-    user_id = int(user_id)
-else:
-    st.write('Please enter a valid User ID')
-    st.stop()
-print(user_id)
+with st.container(height = 500):
+    user_id = st.text_input('Enter User ID here:')
+    if user_id.isnumeric() and int(user_id) > 0:
+        user_id = int(user_id)
+    else:
+        st.write('Please enter a valid User ID')
+        st.stop()
+    print(user_id)
 
-# Load the data
-ratings = pd.read_csv('ratings.csv')
-movies = pd.read_csv('merge_movies.csv')
+    # Load the data
+    ratings = pd.read_csv('ratings.csv')
+    movies = pd.read_csv('merge_movies.csv')
+    movies['genres'] = movies['genres'].apply(lambda x: x.split("|"))
 
-# get top-rated movies watched by user
-top5_user_rated = ratings[ratings['userId'] == user_id].sort_values('rating', ascending=False).head(5)
-st.write(f"Top 5 movies rated by user {user_id}")
+    # get top-rated movies watched by user
+    top5_user_rated = ratings[ratings['userId'] == user_id].sort_values('rating', ascending=False)['movieId'].head(5)
+    st.write(f"Top 5 movies rated by user {user_id}")
+    top5_movies = movies[movies['movieId'].isin(top5_user_rated)]
+
+    # Create a new column 'genres' with space-separated genres
+    top5_movies['genres'] = top5_movies['genres'].apply(lambda x: ", ".join(x))
+
+    # Display the data using st.table
+    st.table(top5_movies[['title', 'genres']])
+
+    # toggle between content-based and collaborative filtering
+    method = st.radio("Choose a recommendation method",
+                   ("Content based filtering", "Collaborative filtering"))
 
 # get top-rated movies watched by user
 recommender = Recommender(ratings_path='train_data.csv', 
                           movies_path='movies.csv', 
                           movie_features_path='movie_features.pkl')
 
+
 # get the recommendations
-movies_to_rec = recommender.recommend_movies(user_id, method="content_based", k=5)
+method = "content_based" if method == "Content based filtering" else "colab_filter"
+movies_to_rec = recommender.recommend_movies(user_id, method=method, k=5)
 
 # display the recommended movies
 st.write(f"Recommended movies for user {user_id}")
@@ -52,7 +67,7 @@ for idx, row in top5_movies_df.iterrows():
     posters.append(fetch_poster(row['tmdbId']))
     genres.append(row['genres'])
 
-st.write(top5_movies_df)
+# st.write(top5_movies_df)
 
 placeholder_image = Image.open('placeholder2.png')
 cols = [col for col in st.columns(len(titles))]
@@ -67,7 +82,7 @@ for i in range(0, len(titles)):
             st.image(placeholder_image, use_column_width=True)
         id = row['movieId']
         st.write("________")
-        
+        genres[i] = ", ".join(genres[i])
         st.write(
             f'<b style="color:#DB4437">Genres</b>:<b> {genres[i]}</b>',
             unsafe_allow_html=True,
